856eef282d130a21e18c53b97872260283cffac9
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-19 15:48:07 +0900

    unmarshal and marshal of json

--- main.go
+++ main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-
-	"github.com/psyto/go-web-app/helpers"
 )
 
-const numPool = 1000
-
-func CalculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChan <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
 }
 
 func main() {
-	intChan := make(chan int)
-	defer close(intChan)
+	myJson := `[
+		{
+			"first_name": "Hiroyuki",
+			"last_name": "Saito",
+			"age": 53
+		},
+		{
+			"first_name": "Amane",
+			"last_name": "Saito",
+			"age": 5
+		}
+	]`
 
-	go CalculateValue(intChan)
+	var unmarshalled []Person
 
-	num := <-intChan
-	log.Println(num)
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+
+	log.Printf("Unmarshalled: %v", unmarshalled)
+
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Mirai"
+	m1.LastName = "Saito"
+	m1.Age = 11
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Mayumi"
+	m2.LastName = "Saito"
+	m2.Age = 44
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "    ")
+
+	if err != nil {
+		log.Println("Error marshalling json", err)
+	}
+
+	fmt.Println(string(newJson))
 }
