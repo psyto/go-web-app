2497517cf16a9397756835a566d5bd2274a938c5
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-19 16:27:45 +0900

    go test

--- main.go
+++ main.go
@@ -1,62 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 	"log"
 )
 
-type Person struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Age       int    `json:"age"`
+func main() {
+	result, err := divide(1, 3)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Result of division is", result)
 }
 
-func main() {
-	myJson := `[
-		{
-			"first_name": "Hiroyuki",
-			"last_name": "Saito",
-			"age": 53
-		},
-		{
-			"first_name": "Amane",
-			"last_name": "Saito",
-			"age": 5
-		}
-	]`
-
-	var unmarshalled []Person
-
-	err := json.Unmarshal([]byte(myJson), &unmarshalled)
-
-	if err != nil {
-		log.Println("Error unmarshalling json", err)
+func divide(x, y float32) (float32, error) {
+	var result float32
+	if y == 0 {
+		return result, errors.New("cannot divide by zero")
 	}
-
-	log.Printf("Unmarshalled: %v", unmarshalled)
-
-	var mySlice []Person
-
-	var m1 Person
-	m1.FirstName = "Mirai"
-	m1.LastName = "Saito"
-	m1.Age = 11
-
-	mySlice = append(mySlice, m1)
-
-	var m2 Person
-	m2.FirstName = "Mayumi"
-	m2.LastName = "Saito"
-	m2.Age = 44
-
-	mySlice = append(mySlice, m2)
-
-	newJson, err := json.MarshalIndent(mySlice, "", "    ")
-
-	if err != nil {
-		log.Println("Error marshalling json", err)
-	}
-
-	fmt.Println(string(newJson))
+	result = x / y
+	return result, nil
 }
