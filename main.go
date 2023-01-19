db50da9bfc3c4e4fe0d9d78f027e9ecb0c57a4d9
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-19 14:17:14 +0900

    something wrong with pointer

--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import "log"
 type Account struct {
 	FirstName string
 	LastName  string
+	Balance   int
+}
+
+type Balance interface {
+	GetBalance() int
+	IncreaseBalance()
+}
+
+func (a *Account) GetBalance() int {
+	return a.Balance
+}
+
+func (a *Account) IncreaseBalance() {
+	a.Balance = a.Balance + 50
+	log.Println(a.Balance)
 }
 
 func main() {
@@ -13,15 +28,21 @@ func main() {
 	accList = append(accList, Account{
 		FirstName: "Hiroyuki",
 		LastName:  "Saito",
+		Balance:   0,
 	})
 
 	accList = append(accList, Account{
 		FirstName: "Amane",
 		LastName:  "Saito",
+		Balance:   100,
 	})
 
-	for i, name := range accList {
-		log.Println(i, name)
+	for _, acc := range accList {
+		acc.IncreaseBalance()
+	}
+
+	for i, acc := range accList {
+		log.Println(i, acc.FirstName, acc.GetBalance())
 	}
 
 }
