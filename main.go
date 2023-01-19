9ccb57579c0775fdb7319a4a37b137737e16f5bd
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-19 18:40:15 +0900

    initial version of web page

--- main.go
+++ main.go
@@ -5,14 +5,30 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Number of bytes written: %d\n", n)
-	})
+const portNumber = ":8080"
 
-	http.ListenAndServe(":8080", nil)
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, "This is the about page and 2 + 2 is %d", sum)
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
