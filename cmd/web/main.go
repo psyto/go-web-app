3502cb9431283ee2c060fb1bc0000497bac095c3
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-20 10:02:28 +0900

    refactor by using go module

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/psyto/go-web-app/pkg/handlers"
 )
 
 const portNumber = ":8080"
@@ -10,8 +12,8 @@ const portNumber = ":8080"
 // main is the main application function
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting application on port %s", portNumber)
 	http.ListenAndServe(portNumber, nil)
