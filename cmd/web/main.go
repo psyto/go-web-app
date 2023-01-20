75ca3f4a7956247f020910a2e6ad673ba02620fe
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-01-20 20:26:32 +0900

    application wide config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/psyto/go-web-app/pkg/config"
 	"github.com/psyto/go-web-app/pkg/handlers"
+	"github.com/psyto/go-web-app/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
